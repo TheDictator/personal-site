614b89bec8336ac53d43d18e2ee022e6956272b1
Kevin Carpenter <kevin@grayboxpdx.com>
2018-08-01 22:09:38 -0700

    Full screen Contact form and other centering fixes and container tweaks

--- public_html/assets/js/slider-nav.js
+++ public_html/assets/js/slider-nav.js
@@ -17,7 +17,6 @@ class StickyNavigation {
         let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
         $('html, body').animate({ scrollTop: scrollTop }, 600);
     }
-
     onScroll() {
         this.checkTabContainerPosition();
         this.findCurrentTabSelector();
